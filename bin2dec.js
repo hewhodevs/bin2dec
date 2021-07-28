5479ba2e39df4004128b155f420c206c8550d688
hewhodevs <hewhodevs@outlook.com>
2021-07-28 18:19:16 +0930

    warnings for no input or invalid input

--- bin2dec.js
+++ bin2dec.js
@@ -1,22 +1,32 @@
 // elements
+const inputLabel = document.getElementById('inputLabel');
+const inputLabelOneZero=document.getElementById('inputLabelOneZero');
 const userInput = document.getElementById('userInput');
 const convertButton = document.getElementById('convertButton');
 const output = document.getElementById('output');
+const textWarning = "warning";
+const inputWarning = "inputWarning";
 
 // event listenters
 convertButton.addEventListener('click', convert);
 
 // Functions
+function getInput() {
+  return userInput.value;
+}
 /* 
-  convert
-  If input is valid, convert it
-  else will warn the user to correct their input
+  Functions to add / remove warning styles
 */
-function convert() {
-  const input = userInput.value;
-  if (isValid(input)) { 
-    displayResult(bin2dec(input));
-  }
+function addClass(e, className) {
+  e.classList.add(className);
+}
+function removeClass(e, className) {
+  e.classList.remove(className);
+}
+function clearWarnings() {
+  removeClass(inputLabel, "warning");
+  removeClass(inputLabelOneZero, "warning");
+  removeClass(userInput, "inputWarning");
 }
 /* 
   isValid(string)
@@ -47,4 +57,27 @@ function bin2dec(inputString) {
 */
 function displayResult(result) {
   output.innerHTML = result;
+}
+/* 
+  convert
+  If input is valid, convert it
+  else will warn the user to correct their input
+*/
+function convert() {
+  const input = getInput();
+  if (input.length === 0) {
+    // Warn when no input provided
+    clearWarnings();
+    addClass(inputLabel, textWarning);
+    addClass(userInput, inputWarning);
+  } else if (isValid(input) === false) {
+    // Warn when input is invalid
+    clearWarnings();
+    addClass(inputLabelOneZero, textWarning);
+    addClass(userInput, inputWarning);
+  } else {
+    // perform conversion of valid input
+    clearWarnings();
+    displayResult(bin2dec(input));
+  }
 }

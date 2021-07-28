2e70345a052d114814822cee908bdccabd83e919
hewhodevs <hewhodevs@outlook.com>
2021-07-28 15:39:36 +0930

    added comments to methods

--- bin2dec.js
+++ bin2dec.js
@@ -3,14 +3,25 @@ const userInput = document.getElementById('userInput');
 const convertButton = document.getElementById('convertButton');
 
 // event listenters
-convertButton.addEventListener('click', function() {
-  let input = userInput.value;
-  if (isValid(input)) { 
-    bin2dec(input) 
-  }
-  // run bin2dec function here
-});
+convertButton.addEventListener('click', convert);
 
+// Functions
+/* 
+  convert
+  If input is valid, convert it
+  else will warn the user to correct their input
+*/
+function convert() {
+  const input = userInput.value;
+  if (isValid(input)) { 
+    bin2dec(input);
+  }
+}
+/* 
+  isValid(string)
+  returns true if the string only contains 1's and 0's
+  returns false otherwise.
+*/
 function isValid(input) {
   const validChars = ['1', '0'];
   for (let i = 0; i <input.length; i++) {
@@ -20,7 +31,11 @@ function isValid(input) {
     }
   } return true;
 }
-
+/* 
+  bin2dec(string)
+  takes any string consisting of 1's and 0's only
+  and converts it's corresponding Integer value;
+*/
 function bin2dec(inputString) {
   let decimal = parseInt(inputString, 2);
   return decimal;
